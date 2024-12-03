ea5d6457dfe2c10bd9db6bd995a363d24ae4342e
SalamSki <slome.lord123@gmail.com>
2024-12-04 00:47:03 +0100

    NER annotation

--- parse_ner_test.py
+++ parse_ner_test.py
@@ -1,29 +1,30 @@
 import json
 import random
 
-with open('ner_entries.json') as f:
-    d = json.load(f)
-    d['words'] = []
-    for entry in d['E1']:
-        d['words'].append(entry)
-    for entry in d['E2']:
-        d['words'].append(entry)
-    for entry in d['E3']:
-        d['words'].append(entry)
-    for entry in d['E4']:
-        d['words'].append(entry)
-
-    d['words'] = list(map(lambda entry: {"definition": entry['definition'], "type": entry['type']}, d['words']))
-
-    entries = sorted(d['words'], key=lambda x: random.random())
-    others = [entry for entry in entries if entry['type'] == 0]
-    locations = [entry for entry in entries if entry['type'] == 1]
-    people = [entry for entry in entries if entry['type'] == 2]
+with open('ner_entries.json', 'r', encoding='utf-8') as NER_FILE:
+    db = json.load(NER_FILE)
+    NER_FILE.close()
     
-    output = {}
-    output['data'] = []
-    output['data'].append(others[:333])
-    output['data'].append(locations[:333])
-    output['data'].append(people[:333])
-    with open('data.json', 'w', encoding='utf-8') as f:
-        json.dump(output, f, ensure_ascii=False, indent=4)
+    flattend_words = []
+    for edition in ['E1', 'E2', 'E3', 'E4']:
+        flattend_words+=db[edition]
+
+    flattend_words = list(map(lambda entry: {"definition": entry['definition'], "headword": entry['headword'], "type": entry['type']}, flattend_words))
+
+    random.shuffle(flattend_words)
+    others = [entry for entry in flattend_words if entry['type'] == 0]
+    locations = [entry for entry in flattend_words if entry['type'] == 1]
+    people = [entry for entry in flattend_words if entry['type'] == 2]
+
+    print(len(others), len(locations), len(people))    
+    
+    for i in range(1,5):
+        output = []
+        i_start = (i-1) * 500
+        i_end = i * 500
+        output.append(others[i_start:i_end])
+        output.append(locations[i_start:i_end])
+        output.append(people[i_start:i_end])
+        with open(f'./dataset/NER/batch_{i}.json', 'w', encoding='utf-8') as SAMPLE_OUT:
+            json.dump(output, SAMPLE_OUT, indent=2, ensure_ascii=False)
+            SAMPLE_OUT.close()
